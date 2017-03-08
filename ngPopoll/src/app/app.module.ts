266908eb72054ca1cf34268a4d4aa5c50c26dd7b
Captp <vincent_mot@hotmail.fr>
2017-03-08 16:29:46 +0100

    Added Chart.js & ng2-charts & chart demo example

--- ngPopoll/src/app/app.module.ts
+++ ngPopoll/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 import { AppComponent } from './app.component';
+
+import { DoughnutChartDemoComponent } from "./demo/doughnut-chart-demo";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoughnutChartDemoComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     FormsModule,
     HttpModule
   ],
---
+++ ngPopoll/src/app/demo/doughnut-chart-demo.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doughnut-chart-demo',
+  templateUrl: './doughnut-chart-demo.html'
+})
+export class DoughnutChartDemoComponent {
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: number[] = [ 350, 450, 100 ];
+  public doughnutChartType: string = 'doughnut';
+
+  // Events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+}
