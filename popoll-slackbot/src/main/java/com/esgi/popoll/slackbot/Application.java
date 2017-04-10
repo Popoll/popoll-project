bc23b01060cacd0b2799106c26ce0173ca6a192b
Mathieu Boisnard <m.boisnard@free.fr>
2017-04-10 23:16:31 +0200

    Add Slackbot spring cloud configuration

--- popoll-slackbot/src/main/java/com/esgi/popoll/slackbot/Application.java
+++ popoll-slackbot/src/main/java/com/esgi/popoll/slackbot/Application.java
@@ -2,8 +2,10 @@ package com.esgi.popoll.slackbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 	
 	public static void main(final String[] args) {
