22929a66df70b3c6a3f49360ad20f91c11ffbfb4
CaptpBdcht <vincent_mot@hotmail.fr>
2017-04-16 17:50:12 +0200

    Working mock

--- ngPopoll/src/app/app.module.ts
+++ ngPopoll/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 import { AppComponent } from './app.component';
+
+// Demo
+import { DoughnutChartDemoComponent } from './demo/doughnut-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Demo
+    DoughnutChartDemoComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     FormsModule,
     HttpModule
   ],
---
+++ ngPopoll/src/app/demo/doughnut-chart.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'doughnut-chart-demo',
+  templateUrl: './doughnut-chart.component.html'
+})
+export class DoughnutChartDemoComponent {
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales'
+  ];
+  public doughnutChartData: number[] = [ 350, 450, 100 ];
+  public doughnutChartType: string = 'doughnut';
+
+  // Events
+  public chartClicked(e: any): void {
+    // console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    // console.log(e);
+  }
+}
